1ad279e96e9476691b32a4444d7d52fddc64b37a
Janus85 <156896153+Zzzzyear@users.noreply.github.com>
2025-12-13 21:14:12 +0800

    Import hybrid reward manager in __init__.py

--- verl/workers/reward_manager/__init__.py
+++ verl/workers/reward_manager/__init__.py
@@ -17,6 +17,7 @@ from .batch import BatchRewardManager
 from .dapo import DAPORewardManager
 from .naive import NaiveRewardManager
 from .prime import PrimeRewardManager
+from . import hybrid
 
 # Note(haibin.lin): no need to include all reward managers here in case of complicated dependencies
 __all__ = [
